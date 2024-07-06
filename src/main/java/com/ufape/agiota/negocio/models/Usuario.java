de17dfe62d4ae1d809a67be7aaa0a1c2b6c7b7f1
Caio <caio.vsg@hotmail.com>
2024-07-06 15:16:59 -0300

    Criação das tabelas de Cliente e Usuario

---
+++ src/main/java/com/ufape/agiota/negocio/models/Usuario.java
@@ -0,0 +1,23 @@
+package com.ufape.agiota.negocio.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    @OneToOne
+    private Endereco endereco;
+}
