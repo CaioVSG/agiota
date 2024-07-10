e5d9499580198656375dd2fea0f2ead894e7d476
Caio <caio.vsg@hotmail.com>
2024-07-10 17:00:59 -0300

    Fixing import errors

--- src/main/java/com/ufape/agiota/comunication/dto/borrowing/BorrowingRequest.java
+++ src/main/java/com/ufape/agiota/comunication/dto/borrowing/BorrowingRequest.java
@@ -1,10 +1,12 @@
 package com.ufape.agiota.comunication.dto.borrowing;
 
-import com.ufape.agiota.comunication.dto.customer.CustomerRequest;
+import com.ufape.agiota.config.SpringApplicationContext;
+import com.ufape.agiota.negocio.models.Borrowing;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.modelmapper.ModelMapper;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -18,5 +20,11 @@ public class BorrowingRequest {
     private Date payday;
     private Double discount;
     private Long userId;
+
+    public Borrowing convertToEntity() {
+        ModelMapper modelMapper = (ModelMapper) SpringApplicationContext.getBean("modelMapper");
+        Borrowing obj = modelMapper.map(this, Borrowing.class);
+        return obj;
+    }
 
 }
