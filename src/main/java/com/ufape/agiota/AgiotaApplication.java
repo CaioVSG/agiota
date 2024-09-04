dd11a1c7717ba2fc0840f688ea12edf3cf5bca54
Caio <caio.vsg@hotmail.com>
2024-09-04 11:31:54 -0300

    Adição do docker-compose.yml

--- src/main/java/com/ufape/agiota/AgiotaApplication.java
+++ src/main/java/com/ufape/agiota/AgiotaApplication.java
@@ -1,16 +1,9 @@
 package com.ufape.agiota;
 
-import com.ufape.agiota.negocio.enums.Frequency;
-import com.ufape.agiota.negocio.models.Agiota;
-import com.ufape.agiota.negocio.models.Borrowing;
-import com.ufape.agiota.negocio.models.Customer;
-import com.ufape.agiota.negocio.models.Installments;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 @SpringBootApplication
 public class AgiotaApplication {
