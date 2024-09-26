5ad094936792bd5786f059db4c6894a93f6d645a
Jonas <jonaslabinfo@gmail.com>
2024-09-26 11:29:27 -0300

    Corrigindo AvaliacaoResponse

--- src/main/java/com/ufape/agiota/comunication/dto/Avaliacao/AvaliacaoResponse.java
+++ src/main/java/com/ufape/agiota/comunication/dto/Avaliacao/AvaliacaoResponse.java
@@ -3,9 +3,12 @@ package com.ufape.agiota.comunication.dto.Avaliacao;
 import com.ufape.agiota.comunication.dto.agiota.AgiotaResponse;
 import com.ufape.agiota.comunication.dto.customer.CustomerResponse;
 import com.ufape.agiota.config.SpringApplicationContext;
+import com.ufape.agiota.negocio.enums.Avaliado;
 import com.ufape.agiota.negocio.enums.Status;
 import com.ufape.agiota.negocio.models.Avaliacao;
 import com.ufape.agiota.negocio.models.Borrowing;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,16 +20,10 @@ import java.util.Date;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class AvaliacaoResponse {
-    private Long id;
-    private Double fees;
-    private BigDecimal value;
-    private Integer numberInstallments;
-    private Date initialDate;
-    private int payday;
-    private Double discount;
-    private Status status;
-    private AgiotaResponse agiota;
-    private CustomerResponse customer;
+
+    private int nota;
+    @Enumerated(EnumType.STRING)
+    private Avaliado avaliado;
 
     public AvaliacaoResponse(Avaliacao obj) {
         if (obj == null) throw new IllegalArgumentException("Objeto não pode ser nulo");
